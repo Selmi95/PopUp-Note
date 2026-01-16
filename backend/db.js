8cf334dcd390066f14161d7fc575d25face10dde
Selmi95 <156472233+Selmi95@users.noreply.github.com>
2026-01-17 01:43:37 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,8 +5,10 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL must be set. Did you forget to provision a database?');
 }
 
+const isProduction = process.env.NODE_ENV === 'production';
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 async function initDatabase() {
